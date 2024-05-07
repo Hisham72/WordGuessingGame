3ae4db01d6ba16ccbaf61a5c99289ad0635abdb3
hisham72 <hishamgamal5@gmail.com>
2024-05-07 23:22:12 +0300

    Add Button Functionality and Mistake Image

--- WordGuessingGame/Form1.Designer.cs
+++ WordGuessingGame/Form1.Designer.cs
@@ -29,7 +29,54 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            circularButton1 = new CircularButton();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // circularButton1
+            // 
+            circularButton1.Anchor = AnchorStyles.None;
+            circularButton1.BackColor = Color.DarkTurquoise;
+            circularButton1.FlatAppearance.BorderSize = 0;
+            circularButton1.FlatStyle = FlatStyle.Flat;
+            circularButton1.Font = new Font("Simplified Arabic Fixed", 35F, FontStyle.Regular, GraphicsUnit.Point);
+            circularButton1.ForeColor = Color.Black;
+            circularButton1.Location = new Point(956, 582);
+            circularButton1.MaximumSize = new Size(46, 45);
+            circularButton1.Name = "circularButton1";
+            circularButton1.RightToLeft = RightToLeft.No;
+            circularButton1.Size = new Size(46, 45);
+            circularButton1.TabIndex = 0;
+            circularButton1.Text = "أ";
+            circularButton1.UseVisualStyleBackColor = false;
+            circularButton1.Click += Keyboard_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.FlatStyle = FlatStyle.Flat;
+            label1.Font = new Font("Simplified Arabic Fixed", 99.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ButtonFace;
+            label1.Location = new Point(557, 355);
+            label1.Name = "label1";
+            label1.Size = new Size(387, 158);
+            label1.TabIndex = 1;
+            label1.Text = "----";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(46, 35);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(248, 270);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
             // 
             // Form1
             // 
@@ -37,13 +84,24 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1444, 881);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Controls.Add(circularButton1);
             Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
             Name = "Form1";
             Text = "خمن الكلمة";
+            WindowState = FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private CircularButton circularButton1;
+        private Label label1;
+        private PictureBox pictureBox1;
     }
 }
--- WordGuessingGame/Form1.cs
+++ WordGuessingGame/Form1.cs
@@ -1,10 +1,67 @@
-namespace WordGuessingGame
+﻿namespace WordGuessingGame
 {
     public partial class Form1 : Form
     {
+        string word = "أرنب";
+        int mistakes = 0;
         public Form1()
         {
             InitializeComponent();
+            label1.Text = new string('-', word.Length);
+        }
+
+        private void Reset()
+        {
+            label1.Text = new string('-', word.Length);
+            pictureBox1.Image = Image.FromFile($"images\\phase1.png");
+            mistakes = 0;
+            /*foreach (var c in this.Controls)
+            {
+                if (c is Button)
+                {
+                    (c as Button).BackColor = Color.DarkTurquoise;
+                    (c as Button).ForeColor = Color.Black;
+                    (c as Button).Enabled = true;
+                }
+            }*/
+            circularButton1.BackColor = Color.DarkTurquoise;
+            circularButton1.ForeColor = Color.Black;
+        }
+
+        private void Keyboard_Click(object sender, EventArgs e)
+        {
+            bool found = false;
+            var labelText = label1.Text.Reverse().ToArray();
+            var senderButton = sender as Button;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] == senderButton.Text[0] && labelText[i] == '-')
+                {
+                    labelText[i] = word[i];
+                    found = true;
+                }
+            }
+            if (!found)
+            {
+                mistakes++;
+                if (mistakes >= 6)
+                {
+                    MessageBox.Show("انتهت اللعبة!\nحاول من جديد.");
+                    Reset();
+                    return;
+                }
+                else
+                {
+                    pictureBox1.Image = Image.FromFile($"images\\phase{mistakes + 1}.png");
+                }
+            }
+            else
+            {
+                label1.Text = new string(labelText.Reverse().ToArray());
+            }
+            senderButton.BackColor = Color.Gray;
+            senderButton.ForeColor = Color.FromArgb(224, 224, 224);
+            //senderButton.Enabled = false;
         }
     }
 }
